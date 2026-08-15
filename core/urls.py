1ae792db90d8e34832b4a532d8657d78c7f5d0b8
Desha <mostafamohamed92895@gmail.com>
2026-08-15 18:55:32 +0300

    core/urls.py: Update core URL configuration

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('pricing/', views.pricing, name='pricing'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('contact/submit/', views.contact_submit, name='contact_submit'),
     path('faq/', views.faq, name='faq'),
     path('robots.txt', views.robots_txt, name='robots'),
     path('sitemap.xml', views.sitemap_xml, name='sitemap'),
