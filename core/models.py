2b99ce8715c0d840bc824bdb3e6e4fef25f5d182
Desha <mostafamohamed92895@gmail.com>
2026-08-15 18:55:25 +0300

    core/models.py: Update core models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactMessage(models.Model):
+    email      = models.EmailField()
+    message    = models.TextField(max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.email} @ {self.created_at:%Y-%m-%d %H:%M}"
