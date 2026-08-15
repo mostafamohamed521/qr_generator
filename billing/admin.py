b62613df53f719b8d6d021436433879005f7ab42
Desha <mostafamohamed92895@gmail.com>
2026-08-15 18:55:07 +0300

    billing/admin.py: Update billing administration configuration

--- billing/admin.py
+++ billing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Plan, Subscription
+from .models import Plan, Subscription, StripeEvent
 
 admin.site.register(Plan)
 admin.site.register(Subscription)
+admin.site.register(StripeEvent)
